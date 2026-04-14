7cabfb8b274b3812a81189140b0c0eef9bd552e4
Avraneel Pal <avraneelpal22@gmail.com>
2026-04-14 15:26:35 +0000

    add reset board function

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,16 @@
 export default class Gameboard {
   constructor() {
+    this.resetBoard();
     this.misses = 0;
     this.allSunk = false;
   }
+
+  resetBoard() {
+    this.board = Array(10);
+    for (let i = 0; i < this.board.length; i++) {
+      this.board[i] = Array(10).fill(0);
+    }
+  }
+
+  placeShip(row, col) {}
 }
